b66dbd3283a59460e65b879d50e59fcf448b2384
LS17ALURA <111367498+LS17ALURA@users.noreply.github.com>
2023-01-13 10:58:49 -0300

    adicionando u console.log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+// mais um console.log de testes:
+console.log("O Paulo fez este commit sem você ver na aula")
+console.log("Enviando para o GitHub")
